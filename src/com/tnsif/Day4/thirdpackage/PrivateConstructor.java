3021450b151c2766f9942eb2de901f00bacede7d
R-Navyashree <r.navyasri04@gmail.com>
2025-10-21 16:16:00 +0530

    Private Constructor

--- src/com/tnsif/Day4/thirdpackage/PrivateConstructor.java
+++ src/com/tnsif/Day4/thirdpackage/PrivateConstructor.java
@@ -3,8 +3,8 @@ package com.tnsif.Day4.thirdpackage;
 public class PrivateConstructor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		MyClass cls= new MyClass();
+		
+		//MyClass cls= new MyClass();
 		
 	}
 
