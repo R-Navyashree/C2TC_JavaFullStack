5aaa534ff44f37fa1fadca4cd183f16c5036b3bd
R-Navyashree <r.navyasri04@gmail.com>
2025-11-11 19:06:46 +0530

    JUnit

---
+++ src/com/tns/junitex/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.tns.junitex;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+	@Test
+	void test()
+	{
+		System.out.println("this is the first test case");
+	}
+	
+
+}
